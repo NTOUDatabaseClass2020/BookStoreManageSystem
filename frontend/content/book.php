b89a7f394b2866796f4d41fb9f7fc5f8708d5d19
Flexolk <aci5722@gmail.com>
2020-12-27 18:37:51 +0800

    書店搜尋 修改樣式

--- frontend/content/book.php
+++ frontend/content/book.php
@@ -1,253 +1,262 @@
 <?php
-	include("../function/condb.php");
+include("../function/condb.php");
 ?>
 <html>
+
 <head>
-  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-  <title>index.php</title>
-  <style>
-	body {
-		margin: 0px;
-	}
-	a {
-		text-decoration: none;
-		font-family: 微軟正黑體,新細明體,標楷體;
-		font-weight: bold;
-		font-size: 17px;
-	}
-	
-	.menu {
-		position:fixed;
-		width: 100%;
-		height: 40px;
-		background-color: dimgrey;
-		z-index: 9999999;
-	}
-	
-	.menu a {
-		text-decoration: none;
-		color: white;
-		font-family: 微軟正黑體,新細明體,標楷體;
-		font-weight: bold;
-		font-size: 17px;
-	}
-	
-	.menu_css {
-		float: left;
-		width: 70%;
-		height: inherit;
-		overflow: hidden;
-		font-family: 微軟正黑體,新細明體,標楷體;
-		font-weight: bold;
-		font-size: 17px;
-		color: white;
-		border-spacing: 0px;
-	}
-	.menu_css tr {
-		display: block;
-	}
-	.menu_css td {
-		height: 40px;
-		padding: 0px 15px 0px 15px;
-		white-space: nowrap;
-	}
-	.menu_css td:hover {
-		background-color: black;
-	}
-	
-	.menu_search{
-		width: 30%;
-		height: inherit;
-		white-space: nowrap;
-		overflow: hidden;
-		font-family: 微軟正黑體,新細明體,標楷體;
-		font-weight: bold;
-		font-size: 17px;
-		color: white;
-	}
-	.menu_search tr {
-		display: block;
-	}
-	.menu_search td {
-		height: 40px;
-		padding: 0px 15px 0px 15px;
-	}
-	.menu_search td:hover {
-		background-color: black;
-	}
-	
-	.content {
-		position: relative;
-		word-wrap: break-word;
-		width: 100%;
-		top: 40px;
-		background-color: #f1f1f1;
-	}
-	
-	.inner_content {
-		padding: 50px 130px 220px 130px;
-	}
-	
-	.inner_content table {
-		background-color: white;
-	}
-	
-	.bimg{
-        max-width:100px;
-        max-height:100px;
-    }
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>index.php</title>
+	<style>
+		body {
+			margin: 0px;
+		}
 
-	li img {
-		width: 100%;
-		height: 100%;
-	}
-	
-	input[type=text] {
-		color: black;
-	}
-	
-	form {
-		margin-bottom: 0em;
-	}
+		a {
+			text-decoration: none;
+			font-family: 微軟正黑體, 新細明體, 標楷體;
+			font-weight: bold;
+			font-size: 17px;
+		}
 
-	#search_icon{
-		/* width: auto; */
-		height: 30px;
-		border: #f1f1f1;
-	}
-  </style>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-  <script src="js/jquery.min.js"></script>
-  <script src="js/jquery.bxslider.min.js"></script>
-  <link href="css/jquery.bxslider.min.css" rel="stylesheet" />
-  <script>
-	$(document).ready(function(){
-	  slider = $('.bxslider').bxSlider(
-		  {
-			  pagerCustom: '#bx-pager'
-		  }
-	  );
-	  slider.startAuto();
-	});
-  </script>
+		#title {
+			margin-left: 40px;
+		}
+
+		.menu {
+			position: fixed;
+			width: 100%;
+			height: 40px;
+			background-color: dimgrey;
+			z-index: 9999999;
+		}
+
+		.menu a {
+			text-decoration: none;
+			color: white;
+			font-family: 微軟正黑體, 新細明體, 標楷體;
+			font-weight: bold;
+			font-size: 17px;
+		}
+
+		.menu_css {
+			float: left;
+			width: 70%;
+			height: inherit;
+			overflow: hidden;
+			font-family: 微軟正黑體, 新細明體, 標楷體;
+			font-weight: bold;
+			font-size: 17px;
+			color: white;
+			border-spacing: 0px;
+		}
+
+		.menu_css tr {
+			display: block;
+		}
+
+		.menu_css td {
+			height: 40px;
+			padding: 0px 15px 0px 15px;
+			white-space: nowrap;
+		}
+
+		.menu_css td:hover {
+			background-color: black;
+		}
+
+		.menu_search {
+			width: 30%;
+			height: inherit;
+			white-space: nowrap;
+			overflow: hidden;
+			font-family: 微軟正黑體, 新細明體, 標楷體;
+			font-weight: bold;
+			font-size: 17px;
+			color: white;
+		}
+
+		.menu_search tr {
+			display: block;
+		}
+
+		.menu_search td {
+			height: 40px;
+			padding: 0px 15px 0px 15px;
+		}
+
+		.menu_search td:hover {
+			background-color: black;
+		}
+
+		.content {
+			position: relative;
+			word-wrap: break-word;
+			width: 100%;
+			top: 40px;
+		}
+
+		.inner_content {
+			padding: 50px 130px 220px 130px;
+		}
+
+		.bimg {
+			max-width: 100px;
+			max-height: 100px;
+		}
+
+		li img {
+			width: 100%;
+			height: 100%;
+		}
+
+		input[type=text] {
+			color: black;
+		}
+
+		form {
+			margin-bottom: 0em;
+		}
+
+		#search_icon {
+			/* width: auto; */
+			height: 30px;
+			border: #f1f1f1;
+		}
+	</style>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+	<script src="js/jquery.min.js"></script>
+	<script src="js/jquery.bxslider.min.js"></script>
+	<link href="css/jquery.bxslider.min.css" rel="stylesheet" />
+	<script>
+		$(document).ready(function() {
+			slider = $('.bxslider').bxSlider({
+				pagerCustom: '#bx-pager'
+			});
+			slider.startAuto();
+		});
+	</script>
 </head>
 
 <body>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a id='title' class="navbar-brand" href="../index.php">BookStore Manage System</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-    <div class="navbar-nav">
-      <a class="nav-item nav-link " href="../index.php">首頁 </a>
-      <a class="nav-item nav-link active" href="bookstore.php">返回書店<span class="sr-only">(current)</span></a>
-    </div>
-  </div>
-</nav>
-<div class="content">
-	<div class="inner_content">
-	</table>
-		<tr class="menu_search">
-			<td>
-				<form method="post" action="book.php?bookstore_id=<?php echo $_GET["bookstore_id"] ?>">
-					<tr>
-						<td>Search</td>
-						<td><input type="text" id="keyword" name="keyword" value="" placeholder="輸入搜尋關鍵字" /></td>
-						<input type="submit" value="送出">
-					</tr>				  
-				</form>
-			</td>
-		</tr>
-		<div style="text-align: left;font-family: &quot;Helvetica Neue&quot;,Helvetica,Arial,sans-serif;font-size: 15px;font-weight: bold;">
-			總數量為: 
-			<?php
+	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+		<a id='title' class="navbar-brand" href="../index.php">BookStore Manage System</a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+			<div class="navbar-nav">
+				<a class="nav-item nav-link " href="../index.php">首頁 </a>
+				<a class="nav-item nav-link active" href="bookstore.php">返回書店<span class="sr-only">(current)</span></a>
+			</div>
+		</div>
+	</nav>
+	<div class="content">
+		<div class="inner_content">
+			</table>
+			<tr class="menu_search">
+				<td>
+					<form method="post" action="book.php?bookstore_id=<?php echo $_GET["bookstore_id"] ?>">
+						<tr>
+							<td> Search</td>
+							<div class="input-group mb-3">
+									<input type="text" id="keyword" class="form-control" name="keyword" value="" placeholder="輸入搜尋關鍵字">
+									<button class="btn btn-outline-secondary" type="submit" id="button-addon2">送出</button>
+							</div>
+						</tr>
+					</form>
+				</td>
+			</tr>
+			<div class="mt-3 mb-3" style="text-align: left;font-family: &quot;Helvetica Neue&quot;,Helvetica,Arial,sans-serif;font-size: 15px;font-weight: bold;">
+				總數量為:
+				<?php
 				$id = $_GET['bookstore_id'];
-				
+
 				$sql = "SELECT COUNT(*) FROM books WHERE bookstore_id = ?";
 				$stmt =  $db->prepare($sql);
 				$error = $stmt->execute(array($id));
-				
-				if($rowcount = $stmt->fetchColumn())
+
+				if ($rowcount = $stmt->fetchColumn())
 					echo $rowcount;
-			?>
-			<button class="btn btn-default"><a href="./book_add.php?bookstore_id=<?php echo $_GET["bookstore_id"]; ?>">新增</a></button>
-			<button class="btn btn-default"><a href="./book_del.php?bookstore_id=<?php echo $_GET["bookstore_id"]; ?>">刪除</a></button>
-			<button class="btn btn-default"><a href="./book_edit.php?bookstore_id=<?php echo $_GET["bookstore_id"]; ?>">修改</a></button>
-		</div>
-		<table class="table"> 
-		  <thead> 
-			<tr> 
-			  <th>#</th> 
-			  <th>id</th> 
-			  <th>book價錢</th> 
-			  <th>book數量</th>
-			  <th>book名稱</th>
-			  <th>book描述</th> 
-			  <th>book種類編號</th>
-			  <th>book種類名稱</th>
-			  <th>封面</th> 
-			  <th></th> 
-			</tr> 
-		  </thead> 
-		  <tbody> 
-		  <?php
-				if (isset($_POST["keyword"]))
-				{
-					$keyword = $_POST["keyword"];
-					if($keyword == ''){
-						$keyword = '%';
-					  }else{
-						$keyword = '%'.$keyword.'%';
-					  }
-					  $id = $_GET['bookstore_id'];
-					  $sql = "SELECT bs.id,bs.price,bs.amount,bs.book_name,bs.description,bs.type,bs.img_url,btl.type_name FROM books bs left join booktype_list btl on bs.type = btl.type where (bs.id like ? or bs.book_name like ? or bs.type like ? or btl.type_name like ?) and bookstore_id=?";
-					  if($stmt = $db->prepare($sql)){
-						  $stmt->execute(array($keyword, $keyword, $keyword, $keyword, $id));
-						  for($rows = $stmt->fetchAll(), $count = 0; $count < count($rows); $count++){
-			  ?>
-						  <tr> 
-							<th scope="row"><?php echo $count;?></th> 
-							<td><?php echo $rows[$count]['id'];?></td> 
-							<td><?php echo $rows[$count]['price'];?></td> 
-							<td><?php echo $rows[$count]['amount'];?></td> 
-							<td><?php echo $rows[$count]['book_name'];?></td> 
-							<td><?php echo $rows[$count]['description'];?></td> 
-							<td><?php echo $rows[$count]['type'];?></td> 
-							<td><?php echo $rows[$count]['type_name'];?></td>
-							<td><img class="bimg" src="<?php echo $rows[$count]['img_url'];?>"></img></td> 
-						  </tr> 
-			  <?php
-						  }		
-					  }
-				}else{
-					$id = $_GET['bookstore_id'];
-					$sql = "SELECT bs.id,bs.price,bs.amount,bs.book_name,bs.description,bs.type,bs.img_url,btl.type_name  FROM books bs left join booktype_list btl on bs.type = btl.type WHERE bookstore_id = ? ";
-					if($stmt = $db->prepare($sql)){
-						$stmt->execute(array($id));
-						
-						for($rows = $stmt->fetchAll(), $count = 0; $count < count($rows); $count++){
-			?>
-					<tr> 
-					  <th scope="row"><?php echo $count;?></th> 
-                      <td><?php echo $rows[$count]['id'];?></td> 
-					  <td><?php echo $rows[$count]['price'];?></a></td> 
-					  <td><?php echo $rows[$count]['amount'];?></td> 
-					  <td><?php echo $rows[$count]['book_name'];?></td> 
-					  <td><?php echo $rows[$count]['description'];?></td> 
-					  <td><?php echo $rows[$count]['type'];?></td>
-					  <td><?php echo $rows[$count]['type_name'];?></td>
-                      <td><img class="bimg" src="<?php echo $rows[$count]['img_url'];?>"></img></td> 
-					</tr> 
-			<?php
+				?>
+				<br>
+				<div class="btn-group" role="group" aria-label="Basic example">
+					<a type="button" class="btn btn-primary" href="./book_add.php?bookstore_id=<?php echo $_GET["bookstore_id"]; ?>">新增</a>
+					<a type="button" class="btn btn-primary" href="./book_del.php?bookstore_id=<?php echo $_GET["bookstore_id"]; ?>">刪除</a>
+					<a type="button" class="btn btn-primary" href="./book_edit.php?bookstore_id=<?php echo $_GET["bookstore_id"]; ?>">修改</a>
+				</div>
+			</div>
+			<table class="table table-bordered table-striped">
+				<thead>
+					<tr>
+						<th>#</th>
+						<th>id</th>
+						<th>book價錢</th>
+						<th>book數量</th>
+						<th>book名稱</th>
+						<th>book描述</th>
+						<th>book種類編號</th>
+						<th>book種類名稱</th>
+						<th>封面</th>
+					</tr>
+				</thead>
+				<tbody>
+					<?php
+					if (isset($_POST["keyword"])) {
+						$keyword = $_POST["keyword"];
+						if ($keyword == '') {
+							$keyword = '%';
+						} else {
+							$keyword = '%' . $keyword . '%';
+						}
+						$id = $_GET['bookstore_id'];
+						$sql = "SELECT bs.id,bs.price,bs.amount,bs.book_name,bs.description,bs.type,bs.img_url,btl.type_name FROM books bs left join booktype_list btl on bs.type = btl.type where (bs.id like ? or bs.book_name like ? or bs.type like ? or btl.type_name like ?) and bookstore_id=?";
+						if ($stmt = $db->prepare($sql)) {
+							$stmt->execute(array($keyword, $keyword, $keyword, $keyword, $id));
+							for ($rows = $stmt->fetchAll(), $count = 0; $count < count($rows); $count++) {
+					?>
+								<tr>
+									<th scope="row"><?php echo $count; ?></th>
+									<td><?php echo $rows[$count]['id']; ?></td>
+									<td><?php echo $rows[$count]['price']; ?></td>
+									<td><?php echo $rows[$count]['amount']; ?></td>
+									<td><?php echo $rows[$count]['book_name']; ?></td>
+									<td><?php echo $rows[$count]['description']; ?></td>
+									<td><?php echo $rows[$count]['type']; ?></td>
+									<td><?php echo $rows[$count]['type_name']; ?></td>
+									<td><img class="bimg" src="<?php echo $rows[$count]['img_url']; ?>"></img></td>
+								</tr>
+							<?php
+							}
+						}
+					} else {
+						$id = $_GET['bookstore_id'];
+						$sql = "SELECT bs.id,bs.price,bs.amount,bs.book_name,bs.description,bs.type,bs.img_url,btl.type_name  FROM books bs left join booktype_list btl on bs.type = btl.type WHERE bookstore_id = ? ";
+						if ($stmt = $db->prepare($sql)) {
+							$stmt->execute(array($id));
+
+							for ($rows = $stmt->fetchAll(), $count = 0; $count < count($rows); $count++) {
+							?>
+								<tr>
+									<th scope="row"><?php echo $count; ?></th>
+									<td><?php echo $rows[$count]['id']; ?></td>
+									<td><?php echo $rows[$count]['price']; ?></a></td>
+									<td><?php echo $rows[$count]['amount']; ?></td>
+									<td><?php echo $rows[$count]['book_name']; ?></td>
+									<td><?php echo $rows[$count]['description']; ?></td>
+									<td><?php echo $rows[$count]['type']; ?></td>
+									<td><?php echo $rows[$count]['type_name']; ?></td>
+									<td><img class="bimg" src="<?php echo $rows[$count]['img_url']; ?>"></img></td>
+								</tr>
+					<?php
+							}
 						}
 					}
-				}
-			?>
-		  </tbody> 
-		</table>
+					?>
+				</tbody>
+			</table>
+		</div>
 	</div>
-</div>
 </body>
+
 </html>
--- frontend/content/book_add.php
+++ frontend/content/book_add.php
@@ -1,192 +1,190 @@
 <?php
-	include("../function/condb.php");
+include("../function/condb.php");
 ?>
 <html>
+
 <head>
-  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-  <title>index.php</title>
-  <style>
-	body {
-		margin: 0px;
-	}
-	a {
-		text-decoration: none;
-		font-family: 微軟正黑體,新細明體,標楷體;
-		font-weight: bold;
-		font-size: 17px;
-	}
-	
-	.menu {
-		position:fixed;
-		width: 100%;
-		height: 40px;
-		background-color: dimgrey;
-		z-index: 9999999;
-	}
-	
-	.menu a {
-		text-decoration: none;
-		color: white;
-		font-family: 微軟正黑體,新細明體,標楷體;
-		font-weight: bold;
-		font-size: 17px;
-	}
-	
-	.menu_css {
-		float: left;
-		width: 100%;
-		height: inherit;
-		overflow: hidden;
-		font-family: 微軟正黑體,新細明體,標楷體;
-		font-weight: bold;
-		font-size: 17px;
-		color: white;
-		border-spacing: 0px;
-	}
-	.menu_css tr {
-		display: block;
-	}
-	.menu_css td {
-		height: 40px;
-		padding: 0px 15px 0px 15px;
-		white-space: nowrap;
-	}
-	.menu_css td:hover {
-		background-color: black;
-	}
-	
-	.content {
-		position: relative;
-		word-wrap: break-word;
-		width: 100%;
-		top: 40px;
-		background-color: #f1f1f1;
-	}
-	
-	.inner_content {
-		padding: 50px 130px 220px 130px;
-	}
-	
-	.inner_content table {
-		background-color: white;
-	}
-	
-	li img {
-		width: 100%;
-		height: 100%;
-	}
-	
-	input[type=text] {
-		color: black;
-	}
-	
-	form {
-		margin-bottom: 0em;
-	}
-	
-  </style>
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-  <script src="js/jquery.min.js"></script>
-  <script src="js/jquery.bxslider.min.js"></script>
-  <link href="css/jquery.bxslider.min.css" rel="stylesheet" />
-  <script>
-	$(document).ready(function(){
-	  slider = $('.bxslider').bxSlider(
-		  {
-			  pagerCustom: '#bx-pager'
-		  }
-	  );
-	  slider.startAuto();
-	});
-  </script>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>index.php</title>
+	<style>
+		body {
+			margin: 0px;
+		}
 
-<script type="text/javascript">
-var $theid = 1
-function addrow($id)
-    {
-		var $idname = "#"+$id;
-		$theid += 1; 
-      $($idname).append('<tr id="'+$theid+'"> '+
-						'<th></th>'+
-						'<td><input type="text" id="book_Price" name="book_Price[]" value=""/></td> '+
-						'<td><input type="text" id="book_Amount" name="book_Amount[]" value=""/></td>'+ 
-						'<td><input type="text" id="book_Name" name="book_Name[]" value=""/></td> '+
-						'<td><input type="text" id="book_Description" name="book_Description[]" value=""/></td>'+ 
-						'<td><input type="text" id="book_Type" name="book_Type[]" value=""/></td>'+ 
-						'<td><input type="text" id="img_url" name="img_url[]" value=""/></td>'+ 
-					'</tr>');  
-			   
-			  
-	}
-	
-function delrow()
-{
-	if($theid>1){
-		
-		var $idname = "#"+$theid;
-	$($idname).remove();
-		$theid -= 1; 
-	}
-	
-		
-}
-//   $("a[id='del_file[]']").click(function(){
-//       if (confirm('確定刪除檔案')) {
-//         return true;
-//       }
-//       return false;
-//   });    
+		a {
+			text-decoration: none;
+			font-family: 微軟正黑體, 新細明體, 標楷體;
+			font-weight: bold;
+			font-size: 17px;
+		}
 
- </script>
+		.menu {
+			position: fixed;
+			width: 100%;
+			height: 40px;
+			background-color: dimgrey;
+			z-index: 9999999;
+		}
+
+		.menu a {
+			text-decoration: none;
+			color: white;
+			font-family: 微軟正黑體, 新細明體, 標楷體;
+			font-weight: bold;
+			font-size: 17px;
+		}
+
+		.menu_css {
+			float: left;
+			width: 100%;
+			height: inherit;
+			overflow: hidden;
+			font-family: 微軟正黑體, 新細明體, 標楷體;
+			font-weight: bold;
+			font-size: 17px;
+			color: white;
+			border-spacing: 0px;
+		}
+
+		.menu_css tr {
+			display: block;
+		}
+
+		.menu_css td {
+			height: 40px;
+			padding: 0px 15px 0px 15px;
+			white-space: nowrap;
+		}
+
+		.menu_css td:hover {
+			background-color: black;
+		}
+
+	
+
+		.inner_content {
+			padding: 50px 130px 220px 130px;
+		}
+
+		.inner_content table {
+			background-color: white;
+		}
+
+		li img {
+			width: 100%;
+			height: 100%;
+		}
+
+		input[type=text] {
+			color: black;
+		}
+
+		form {
+			margin-bottom: 0em;
+		}
+	</style>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+	<script src="js/jquery.min.js"></script>
+	<script src="js/jquery.bxslider.min.js"></script>
+	<link href="css/jquery.bxslider.min.css" rel="stylesheet" />
+	<script>
+		$(document).ready(function() {
+			slider = $('.bxslider').bxSlider({
+				pagerCustom: '#bx-pager'
+			});
+			slider.startAuto();
+		});
+	</script>
+
+	<script type="text/javascript">
+		var $theid = 1
+
+		function addrow($id) {
+			var $idname = "#" + $id;
+			$theid += 1;
+			$($idname).append('<tr id="' + $theid + '"> ' +
+				'<th></th>' +
+				'<td><input type="text" id="book_Price" name="book_Price[]" value=""/></td> ' +
+				'<td><input type="text" id="book_Amount" name="book_Amount[]" value=""/></td>' +
+				'<td><input type="text" id="book_Name" name="book_Name[]" value=""/></td> ' +
+				'<td><input type="text" id="book_Description" name="book_Description[]" value=""/></td>' +
+				'<td><input type="text" id="book_Type" name="book_Type[]" value=""/></td>' +
+				'<td><input type="text" id="img_url" name="img_url[]" value=""/></td>' +
+				'</tr>');
+
+
+		}
+
+		function delrow() {
+			if ($theid > 1) {
+
+				var $idname = "#" + $theid;
+				$($idname).remove();
+				$theid -= 1;
+			}
+
+
+		}
+		//   $("a[id='del_file[]']").click(function(){
+		//       if (confirm('確定刪除檔案')) {
+		//         return true;
+		//       }
+		//       return false;
+		//   });    
+	</script>
 </head>
+
 <body>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a id='title' class="navbar-brand" href="../index.php">BookStore Manage System</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-    <div class="navbar-nav">
-      <a class="nav-item nav-link " href="../index.php">首頁 </a>
-      <a class="nav-item nav-link active" href="bookstore.php">返回書店<span class="sr-only">(current)</span></a>
-	  <a class="nav-item nav-link active" href="book.php?bookstore_id=<?php echo $_GET["bookstore_id"]?>">編輯書店<span class="sr-only">(current)</span></a>
-	</div>
-  </div>
-</nav>
-<form method="get" action="book_addsave.php" enctype="multipart/form-data">
-	<div class="content">
-		<div class="inner_content">
-			<input type="button" id="add_button" name="add_button" onclick="addrow('add_file_button')" value="增加附加欄位">
-			<input type="button" id="add_button" name="add_button" onclick="delrow()" value="減少附加欄位">
-			<button type="submit" name="bookstore_id" value="<?php echo $_GET["bookstore_id"]?>" >點我新增</button>
-			<table class="table" >
-			  <thead>
-				<tr> 
-				<th>#</th>
-				<th>book價錢</th> 
-				<th>book數量</th> 
-				<th>book名稱</th> 
-				<th>book描述</th> 
-				<th>book種類</th> 
-				<th>封面</th> 
-				</tr>  
-			  </thead> 
-			  <tbody id="add_file_button">
-					<tr id="1"> 
-						<th></th>
-						<td><input type="text" id="book_Price" name="book_Price[]" value=""/></td> 
-						<td><input type="text" id="book_Amount" name="book_Amount[]" value=""/></td> 
-						<td><input type="text" id="book_Name" name="book_Name[]" value=""/></td> 
-						<td><input type="text" id="book_Description" name="book_Description[]" value=""/></td> 
-						<td><input type="text" id="book_Type" name="book_Type[]" value=""/></td> 
-						<td><input type="text" id="img_url" name="img_url[]" value=""/></td> 
-					</tr>
-			  </tbody> 
-			</table>
+	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+		<a id='title' class="navbar-brand" href="../index.php">BookStore Manage System</a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+			<div class="navbar-nav">
+				<a class="nav-item nav-link " href="../index.php">首頁 </a>
+				<a class="nav-item nav-link active" href="bookstore.php">返回書店<span class="sr-only">(current)</span></a>
+				<a class="nav-item nav-link active" href="book.php?bookstore_id=<?php echo $_GET["bookstore_id"] ?>">編輯書店<span class="sr-only">(current)</span></a>
+			</div>
 		</div>
-	</div>
-</form>
+	</nav>
+	<form method="get" action="book_addsave.php" enctype="multipart/form-data">
+		<div class="content">
+			<div class="inner_content">
+				<div class="btn-group mb-3 " role="group" aria-label="Basic example">
+					<input class="btn btn-primary"  type="button" id="add_button" name="add_button" onclick="addrow('add_file_button')" value="增加附加欄位">
+					<input class="btn btn-primary" type="button" id="add_button" name="add_button" onclick="delrow()" value="減少附加欄位">
+					<button class="btn btn-primary" type="submit" name="bookstore_id" value="<?php echo $_GET["bookstore_id"] ?>">點我新增</button>
+				</div>
+				<table class="table table-bordered table-striped">
+					<thead>
+						<tr>
+							<th>#</th>
+							<th>book價錢</th>
+							<th>book數量</th>
+							<th>book名稱</th>
+							<th>book描述</th>
+							<th>book種類</th>
+							<th>封面</th>
+						</tr>
+					</thead>
+					<tbody id="add_file_button">
+						<tr id="1">
+							<th></th>
+							<td><input type="text" id="book_Price" name="book_Price[]" value="" /></td>
+							<td><input type="text" id="book_Amount" name="book_Amount[]" value="" /></td>
+							<td><input type="text" id="book_Name" name="book_Name[]" value="" /></td>
+							<td><input type="text" id="book_Description" name="book_Description[]" value="" /></td>
+							<td><input type="text" id="book_Type" name="book_Type[]" value="" /></td>
+							<td><input type="text" id="img_url" name="img_url[]" value="" /></td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+		</div>
+	</form>
 </body>
+
 </html>
