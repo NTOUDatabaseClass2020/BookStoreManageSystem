bcb989dfa12d05fe364ec9ea47c04b018532d7af
yingcongleong <64768796+yingcongleong@users.noreply.github.com>
2020-12-29 18:17:00 +0800

    Add files via upload

--- frontend/index.php
+++ frontend/index.php
@@ -1,101 +1,145 @@
 <?php
-	include("function/condb.php");
+include("function/condb.php");
 ?>
 <html>
-<head>
-  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-  <title>index.php</title>
-  <style>
-	body {
-		margin: 0px;
-	}
-	a {
-		text-decoration: none;
-		color: white;
-		font-family: 微軟正黑體,新細明體,標楷體;
-		font-weight: bold;
-		font-size: 17px;
-	}
-	
-	.menu {
-		position:fixed;
-		width: 100%;
-		height: 40px;
-		background-color: dimgrey;
-		z-index: 9999999;
-	}
-	
-	.menu_css {
-		float: left;
-		width: 100%;
-		height: inherit;
-		overflow: hidden;
-		font-family: 微軟正黑體,新細明體,標楷體;
-		font-weight: bold;
-		font-size: 17px;
-		color: white;
-		border-spacing: 0px;
-	}
-	.menu_css tr {
-		display: block;
-	}
-	.menu_css td {
-		height: 40px;
-		padding: 0px 15px 0px 15px;
-		white-space: nowrap;
-	}
-	.menu_css td:hover {
-		background-color: black;
-	}
-	
-	.content {
-		position: relative;
-		word-wrap: break-word;
-		width: 100%;
-		top: 40px;
-	}
 
-	#title{
-		margin-left : 40px;
-	}
-	
-	li img {
-		width: 100%;
-		height: 100%;
-	}
-  </style>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-  <script src="js/jquery.min.js"></script>
-  <script src="js/jquery.bxslider.min.js"></script>
-  <link href="css/jquery.bxslider.min.css" rel="stylesheet" />
-  <script>
-	$(document).ready(function(){
-	  slider = $('.bxslider').bxSlider(
-		  {
-			  pagerCustom: '#bx-pager'
-		  }
-	  );
-	  slider.startAuto();
-	});
-  </script>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>index.php</title>
+	<style>
+		body {
+			margin: 0px;
+		}
+
+		a {
+			text-decoration: none;
+			color: white;
+			font-family: 微軟正黑體, 新細明體, 標楷體;
+			font-weight: bold;
+			font-size: 17px;
+		}
+
+		.menu {
+			position: fixed;
+			width: 100%;
+			height: 40px;
+			background-color: dimgrey;
+			z-index: 9999999;
+		}
+
+		.menu_css {
+			float: left;
+			width: 100%;
+			height: inherit;
+			overflow: hidden;
+			font-family: 微軟正黑體, 新細明體, 標楷體;
+			font-weight: bold;
+			font-size: 17px;
+			color: white;
+			border-spacing: 0px;
+		}
+
+		.menu_css tr {
+			display: block;
+		}
+
+		.menu_css td {
+			height: 40px;
+			padding: 0px 15px 0px 15px;
+			white-space: nowrap;
+		}
+
+		.menu_css td:hover {
+			background-color: black;
+		}
+
+		.img{
+			position: relative;
+			word-wrap: break-word;
+			width: 100%;
+			max-height : 700px;
+		}
+
+
+		#title {
+			margin-left: 40px;
+		}
+
+		li img {
+			width: 100%;
+			height: 100%;
+		}
+	</style>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+	<script src="js/jquery.min.js"></script>
+	<script src="js/jquery.bxslider.min.js"></script>
+	<link href="css/jquery.bxslider.min.css" rel="stylesheet" />
+	<script>
+		$(document).ready(function() {
+			slider = $('.bxslider').bxSlider({
+				pagerCustom: '#bx-pager'
+			});
+			slider.startAuto();
+		});
+	</script>
 </head>
+
 <body>
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a id='title' class="navbar-brand" href="index.php">BookStore Manage System</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-    <div class="navbar-nav">
-      <a class="nav-item nav-link active" href="index.php">首頁 <span class="sr-only">(current)</span></a>
-      <a class="nav-item nav-link" href="content/bookstore.php">書店</a>
-    </div>
-  </div>
-</nav>
-<div class="content">
-	
-</div>
+	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+		<a id='title' class="navbar-brand" href="index.php">BookStore Manage System</a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+			<div class="navbar-nav">
+				<a class="nav-item nav-link active" href="index.php">首頁</a>
+				<a class="nav-item nav-link" href="content/bookstore.php">書店</a>
+			</div>
+		</div>
+	</nav>
+	<div class="content">
+		<div class="inner_content">
+			<div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+				<ol class="carousel-indicators">
+					<li data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active"></li>
+					<li data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1"></li>
+					<li data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2"></li>
+				</ol>
+				<div class="carousel-inner">
+					<div class="carousel-item active">
+					<img class="d-block w-100 img" src="https://www.nippon.com/hk/ncommon/contents/japan-topics/184399/184399.jpg" alt="First slide">
+						<div class="carousel-caption d-none d-md-block">
+							<h5>Bookstore Mangement System</h5>
+							<p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+						</div>
+					</div>
+					<div class="carousel-item">
+					<img class="d-block w-100 img" src="https://cdn-news.readmoo.com/wp-content/uploads/2015/06/150622_%E8%94%A6%E5%B1%8B%E5%AE%B6%E9%9B%BB.jpg" alt="Second slide">						<div class="carousel-caption d-none d-md-block">
+							<h5>Bookstore Mangement System</h5>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+						</div>
+					</div>
+					<div class="carousel-item">
+					<img class="d-block w-100 img" src="https://www.travel.taipei/image/181449/1024x768" alt="Third slide">
+						<div class="carousel-caption d-none d-md-block">
+							<h5>Bookstore Mangement System</h5>
+							<p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+						</div>
+					</div>
+				</div>
+				<a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-bs-slide="prev">
+					<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+					<span class="visually-hidden">Previous</span>
+				</a>
+				<a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-bs-slide="next">
+					<span class="carousel-control-next-icon" aria-hidden="true"></span>
+					<span class="visually-hidden">Next</span>
+				</a>
+			</div>
+		</div>
+	</div>
 </body>
+
 </html>
